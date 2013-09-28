e3f8a9798dafc3d0fa3f980312ea3fe40f942e24
Stella Cotton <stella@stellacotton.com>
2013-09-28 10:14:38 -0700

    Add working file sort function

--- week1project.py
+++ week1project.py
@@ -1,13 +1,22 @@
 import os
+import shutil
 
+#create container folder for alphabet folders
 os.mkdir("alphabet_folder")
 
-for i in range(97,99):
+#create a separate folder for each letter of the alphabet
+for i in range(97,123):
     letter = chr(i)
     os.mkdir("alphabet_folder/%s" % letter)
 
+#create list of alphabet folders and list of user files
 alphabet_list = os.listdir("alphabet_folder")
-zip_files = os.listdir("files")
-print alphabet_list.sort()
-print zip_files
+user_files = os.listdir("originalfiles")
     
+#move each user file into a folder that corresponds to the first
+#letter of the user file
+for a_user_file in zip_files:
+	first_letter = a_user_file[0]
+	src = "originalfiles/%s" % a_user_file
+	dest = "alphabet_folder/%s/%s" % (first_letter, a_user_file)
+	shutil.move(src,dest)
